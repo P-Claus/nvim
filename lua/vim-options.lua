8728719f14dea324b70971af3617b7ce8a35f12b
Pieter Claus <pieter.claus1@gmail.com>
2024-01-03 20:57:02 +0100

    add none-ls and add prettier and eslint for JS

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,24 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+
+
 --Basic settings
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Naviagate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+
